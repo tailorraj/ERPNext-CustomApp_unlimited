0a769bedc88c7e956f2159cca840c5471758cab8
Raj Tailor <tailorraj111@gmail,com>
2019-08-26 16:58:44 +0530

    my first commit

--- unlimited/unlimited_tomorrow/doctype/order/order.py
+++ unlimited/unlimited_tomorrow/doctype/order/order.py
@@ -10,7 +10,24 @@ from frappe import _
 import frappe.defaults
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils import get_request_site_address
 from frappe.utils.password import get_decrypted_password
+import requests
+import googleapiclient.discovery
+import google.oauth2.credentials
+import pytz
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
+# from oauth2client import file,client,tools
+
+
+# redirect_uri = get_request_site_address(True) + "?cmd=unlimited.unlimited_tomorrow.doctype.tenant_order.tenant_order.google_callback"
+redirect_uri = "http://localhost:5050" + "?cmd=unlimited.unlimited_tomorrow.doctype.tenant_order.tenant_order.google_callback"
+SCOPES = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events'
+client_id = frappe.db.get_value("Unlimited Settings", None, "client_id")
+client_secret = frappe.db.get_value("Unlimited Settings", None, "client_secret")
+refresh_token = frappe.db.get_value("Unlimited Settings", None, "refresh_token")
+calendar_id = frappe.db.get_value("Unlimited Settings", None, "calendar_id")
 
 class TenantOrder(Document):
 
@@ -25,50 +42,83 @@ class TenantOrder(Document):
 
 		customer = frappe.db.get_value("User", {"email":str(frappe.session.user)}, "first_name")
 		
-		if not self.customer:
-			# Set customer
-			self.customer = customer
-			frappe.db.set_value("Tenant Order", self.name, "customer", customer)
-			frappe.db.commit()
-		
-			# Set customer name
-			self.customer_name = frappe.db.get_value("Customer", customer, "customer_name")
-			frappe.db.set_value("Tenant Order", self.name, "customer_name", frappe.db.get_value("Customer", customer, "customer_name"))
-			frappe.db.commit()
+		# if not self.customer:
+		# Set customer
+		self.customer = customer
+		frappe.db.set_value("Tenant Order", self.name, "customer", customer)
+		frappe.db.commit()
+	
+		# Set customer name
+		self.customer_name = frappe.db.get_value("Customer", customer, "customer_name")
+		frappe.db.set_value("Tenant Order", self.name, "customer_name", frappe.db.get_value("Customer", customer, "customer_name"))
+		frappe.db.commit()
 
 		default_tenant_employee = frappe.db.get_value("Unlimited Settings", None, "default_tenant_employee")
 		
 
-		if not self.employee:
-			# Set employee
-			self.employee = default_tenant_employee
-			frappe.db.set_value("Tenant Order", self.name, "employee", default_tenant_employee)
-			frappe.db.commit()
+		# if not self.employee:
+		# Set employee
+		self.employee = default_tenant_employee
+		frappe.db.set_value("Tenant Order", self.name, "employee", default_tenant_employee)
+		frappe.db.commit()
 
-			# Set employee name
-			self.employee_name = frappe.db.get_value("Employee", default_tenant_employee, "employee_name")
-			frappe.db.set_value("Tenant Order", self.name, "employee_name", frappe.db.get_value("Employee", default_tenant_employee, "employee_name"))
-			frappe.db.commit()
+		# Set employee name
+		self.employee_name = frappe.db.get_value("Employee", default_tenant_employee, "employee_name")
+		frappe.db.set_value("Tenant Order", self.name, "employee_name", frappe.db.get_value("Employee", default_tenant_employee, "employee_name"))
+		frappe.db.commit()
 
-			# Set employee duration
-			self.duration = frappe.db.get_value("Employee", default_tenant_employee, "time_per_appointment")
-			frappe.db.set_value("Tenant Order", self.name, "duration", frappe.db.get_value("Employee", default_tenant_employee, "time_per_appointment"))
-			frappe.db.commit()
+		# Set employee duration
+		self.duration = frappe.db.get_value("Employee", default_tenant_employee, "time_per_appointment")
+		frappe.db.set_value("Tenant Order", self.name, "duration", frappe.db.get_value("Employee", default_tenant_employee, "time_per_appointment"))
+		frappe.db.commit()
+		
 
 		enter_credit_card_details(self)
 
 		# Assign task to default sales employee
 		if self.workflow_state == "Debug Scan with Client": 
 			assign_task_to_sales_employee(self)
+			access_token = get_access_token()
+			if access_token:
+				# Insert event in google calendar
+				created_calendar_event = insert_events(self,access_token)
+				if created_calendar_event:
+					frappe.msgprint("event set with id"+created_calendar_event["id"])
+					# self.google_event_id = created_calendar_event["id"]
+					# frappe.db.set_value("Tenant Order", self.name, "google_event_id", created_calendar_event["id"])
+					# frappe.db.commit()
+
+					# gcalendar_event_link = "<a href='"+ cstr(created_calendar_event["htmlLink"]) +"' target='_blank'>"+ cstr(created_calendar_event["htmlLink"]) +"</a>"
+
+					# self.google_calendar_event_url = gcalendar_event_link
+					# frappe.db.set_value("Tenant Order", self.name, "google_calendar_event_url", gcalendar_event_link)
+					# frappe.db.commit()
 
 		if not self.appointment_time:
 			self.appointment_time = self.available_time_slot
 			frappe.db.set_value("Tenant Order", self.name, "duration", frappe.db.get_value("Employee", default_tenant_employee, "time_per_appointment"))
 			frappe.db.commit()
 
+		
+		
+
 	def after_insert(self):
 		# Assign task to default modelar employee
 		assign_task_to_modeler_employee(self)
+		access_token = get_access_token()
+		if access_token:
+			# Insert event in google calendar
+			created_calendar_event = insert_events(self,access_token)
+			if created_calendar_event:
+				self.google_event_id = created_calendar_event["id"]
+				frappe.db.set_value("Tenant Order", self.name, "google_event_id", created_calendar_event["id"])
+				frappe.db.commit()
+
+				gcalendar_event_link = "<a href='"+ cstr(created_calendar_event["htmlLink"]) +"' target='_blank'>"+ cstr(created_calendar_event["htmlLink"]) +"</a>"
+
+				self.google_calendar_event_url = gcalendar_event_link
+				frappe.db.set_value("Tenant Order", self.name, "google_calendar_event_url", gcalendar_event_link)
+				frappe.db.commit()
 
 # ---------------------------------------------------------------------------
 # Enter Credit Card Details Into Customer
@@ -114,8 +164,8 @@ def assign_task_to_modeler_employee(self):
 # Assign task to sales employee
 # --------------------------------------------------------------------------- */
 def assign_task_to_sales_employee(self):
-	# default_tenant_employee = frappe.db.get_value("Unlimited Settings", None, "default_tenant_sales_employee")
-	employee_user_id = frappe.db.get_value("Employee", self.employee, "user_id")
+	default_tenant_employee = frappe.db.get_value("Unlimited Settings", None, "default_tenant_sales_employee")
+	employee_user_id = frappe.db.get_value("Employee", default_tenant_employee, "user_id")
 
 	# Assign ToDo to Tenant Employee
 	ToDo = frappe.get_doc({
@@ -327,3 +377,120 @@ def get_credit_card_info(user):
 		"ex_month":cstr(get_decrypted_password('Customer',customer_id,'expiry_month',False)),
 		"ex_year":cstr(get_decrypted_password('Customer',customer_id,'expiry_year',False))
 	}
+
+@frappe.whitelist()
+def google_callback(code=None, state=None, account=None):
+	# frappe.throw(cstr(redirect_uri))
+	if code is None:
+		
+		return {
+			'url': 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&response_type=code&prompt=consent&client_id={}&include_granted_scopes=true&scope={}&redirect_uri={}'.format(client_id, SCOPES, redirect_uri)
+			}
+	else:
+		try:
+			data = {'code': code,
+				'client_id': client_id,
+				'client_secret': client_secret,
+				'redirect_uri': redirect_uri,
+				'grant_type': 'authorization_code'}
+			r = requests.post('https://www.googleapis.com/oauth2/v4/token', data=data).json()
+			frappe.db.set_value("Unlimited Settings", None, "code", code)
+			if 'access_token' in r:
+				frappe.db.set_value("Unlimited Settings", None, "access_token", r['access_token'])
+			if 'refresh_token' in r:
+				frappe.db.set_value("Unlimited Settings", None, "refresh_token", r['refresh_token'])
+			frappe.db.commit()
+			frappe.local.response["type"] = "redirect"
+			frappe.local.response["location"] = "success.html"
+			return
+		except Exception as e:
+			frappe.throw(e.message)
+
+def get_access_token():
+	if not refresh_token:
+		raise frappe.ValidationError(_("GCalendar is not configured."))
+	data = {
+		'client_id': client_id,
+		'client_secret': client_secret,
+		'refresh_token': refresh_token,
+		'grant_type': "refresh_token",
+		'scope': SCOPES
+	}
+	try:
+		r = requests.post('https://www.googleapis.com/oauth2/v4/token', data=data).json()
+	except requests.exceptions.HTTPError:
+		frappe.throw(_("Something went wrong during the token generation. Please request again an authorization code."))
+	if r.get('access_token'):
+		frappe.db.set_value("Unlimited Settings", None, "access_token", cstr(r.get('access_token')))
+		frappe.db.commit()
+	
+	return r.get('access_token')
+
+def delete_event(self,access_token):
+	credentials_dict = {
+	'token': access_token,
+	'refresh_token': refresh_token,
+	'token_uri': 'https://www.googleapis.com/oauth2/v4/token',
+	'client_id': client_id,
+	'client_secret': client_secret,
+	'scopes':SCOPES
+	}
+	credentials = google.oauth2.credentials.Credentials(**credentials_dict)
+	gcalendar = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
+	gcalendar.events().delete(calendarId='primary', eventId=self.google_event_id).execute()
+
+def  insert_events(self,access_token):
+	credentials_dict = {
+	'token': access_token,
+	'refresh_token': refresh_token,
+	'token_uri': 'https://www.googleapis.com/oauth2/v4/token',
+	'client_id': client_id,
+	'client_secret': client_secret,
+	'scopes':SCOPES
+	}
+	
+	modeler_emp_email=frappe.db.get_value("Employee", self.employee, "user_id")
+	
+	customer_email=frappe.db.get_value("User",{"first_name":self.customer},"name")
+	# frappe.msgprint(modeler_emp_email)
+	# frappe.throw(customer_email)
+	time_zone = cstr(pytz.timezone(frappe.db.get_value("System Settings",None,"time_zone")).localize(get_datetime(self.appointment_time)).strftime('%z'))[:3]+':'+cstr(pytz.timezone(frappe.db.get_value("System Settings",None,"time_zone")).localize(get_datetime(self.appointment_time)).strftime('%z'))[3:]
+
+	date_time_string=self.appointment_date+'T'+self.appointment_time+time_zone
+
+	credentials = google.oauth2.credentials.Credentials(**credentials_dict)
+
+	gcalendar = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
+	
+	event = {
+	'summary': cstr(frappe.db.get_value("Global Defaults",None,"default_company")),
+	'location': cstr(frappe.db.get_value("Global Defaults",None,"default_company")),
+	'description': 'Your apointment with Unlimited Tomorrow',
+	'start': {
+		# 'dateTime': start,
+		'dateTime': date_time_string,
+    	'timeZone': cstr(frappe.db.get_value("System Settings",None,"time_zone"))
+	},
+	'end': {
+		# 'dateTime': end,
+		'dateTime': date_time_string,
+    	'timeZone': cstr(frappe.db.get_value("System Settings",None,"time_zone"))
+	},
+	'attendees': [
+
+		{'email': modeler_emp_email,'self':True},
+    	{'email': customer_email,'self':True}
+		# {'email':[modeler_emp_email,customer_email],
+		# 'self':True}
+		# ,
+		# {'email':customer_email,
+		# 'self':True}
+	]
+	}
+
+	try:
+		remote_event = gcalendar.events().insert(calendarId=calendar_id, body=event).execute()
+		return remote_event
+	except Exception:
+		frappe.log_error(frappe.get_traceback(), "Google Calendar Synchronization Error")
+
--- unlimited/unlimited_tomorrow/doctype/unlimited_settings/unlimited_settings.js
+++ unlimited/unlimited_tomorrow/doctype/unlimited_settings/unlimited_settings.js
@@ -12,3 +12,15 @@ cur_frm.fields_dict.default_sales_person.get_query = function(doc,dt,dn) {
 		 filters: { 'is_group': 0 }
 	}
 }
+
+cur_frm.cscript.allow_google_calendar_access= function(doc,dt,dn){
+		frappe.call({
+			method: "unlimited.unlimited_tomorrow.doctype.tenant_order.tenant_order.google_callback",
+			args: {},
+			callback: function(r) {
+				if (r.message){
+					window.open(r.message.url);
+				}
+			}
+		})
+}
